282146b3232095258cd69225352ee7435b7a18f9
aradidaniel <106033926+aradidaniel@users.noreply.github.com>
2024-06-09 12:24:24 +0200

    change .ps1 file

--- docker-run-homework.ps1
+++ docker-run-homework.ps1
@@ -1,9 +1,10 @@
-#build images
+#build images in both 
 
 mvnw clean verify
 
 #build container
-docker build -t homework1:test .
+docker build -t homework:backapp --target backapp .
+docker build -t homework:frontapp --target frontapp .
 
 #check image labels
 
@@ -13,8 +14,8 @@ docker inspect homework1:test
 you can also do it on a running container like this:
 
 #run container
-docker run -p 8080:8080 -it homework1:test
-
+docker run -p 8080:8080 homework:frontapp
+docker run -p 8081:8081 homework:backapp
 #check container labels
 docker inspect "containerID", but 
 

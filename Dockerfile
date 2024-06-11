3177c0c94d51f681dd0f32aedde6019afae3e527
aradidaniel <106033926+aradidaniel@users.noreply.github.com>
2024-06-11 16:41:28 +0200

    done

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,8 @@
-ARG JAR_PATH
-
 FROM quay.io/drsylent/cubix/block2/homework-base:java21 
+
+ARG JAR_PATH
 LABEL cubix.homework.owner="Aradi Daniel"
 ENV CUBIX_HOMEWORK="Aradi Daniel"
 ENV APP_DEFAULT_MESSAGE=""
-WORKDIR ${JAR_PATH}/target/
-COPY *.jar ./app.jar
+COPY ${JAR_PATH}/target/*.jar app.jar
 CMD ["java", "-jar", "app.jar"]
--- docker-run-homework.ps1
+++ docker-run-homework.ps1
@@ -2,36 +2,25 @@
 mvnw clean verify
 
 #build container
-docker build -t homework:backapp --target backapp . --server.port=8081
-docker build -t homework:frontapp --target frontapp . --back.url=${BACK_URL}
-
 docker build --build-arg="JAR_PATH=backapp" -t backapp .
 docker build --build-arg="JAR_PATH=frontapp" -t frontapp .
 
-
-
 #check image labels
-
-docker inspect homework:backapp
-docker inspect homework:frontapp
+docker inspect backapp
+docker inspect frontapp
 
 #run container
-docker run -p 8080:8080 homework:frontapp 
-docker run -p 8080:8080 homework:backapp
-
-
-
+docker run -p 8080:8080 frontapp 
+docker run -p 8080:8080 backapp
 
 #check image logs
-
-docker logs -f homework:frontapp
-docker logs -f homework:backapp
+docker logs -f frontapp
+docker logs -f backapp
 
 #check container labels
 docker inspect "containerID"
 
 #run docker compose
-
 docker-compose up
 
 

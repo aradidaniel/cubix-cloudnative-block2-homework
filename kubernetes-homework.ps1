6961ff6dd7edd956e1f31bb0dbc8addc82713244
aradidaniel <106033926+aradidaniel@users.noreply.github.com>
2024-06-17 08:59:32 +0200

    fix issues

--- kubernetes-homework.ps1
+++ kubernetes-homework.ps1
@@ -1,10 +1,14 @@
 #deployment
-kubectl apply -f backapp.yaml
+kubectl apply -f backapp-deployment.yaml
+kubectl apply -f backapp-service.yaml
+
 kubectl apply -f frontapp.yaml
+kubectl apply -f frontapp-service.yaml
+
 kubectl apply -f frontapp-ingress.yaml
 
 #check logs for backapp
-kubectl logs backapp-deployment-b6c946688-6ggnv
+kubectl logs -l homework=backapp
 
 #list all resources related to frontapp
 kubectl get all -l homework=frontapp

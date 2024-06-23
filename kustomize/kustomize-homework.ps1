b0e0ac9cc8c75a3e4c6abdb1d0adc770a036a8c3
aradidaniel <106033926+aradidaniel@users.noreply.github.com>
2024-06-23 21:02:03 +0200

    fix helm and kustomize issues

--- kustomize/kustomize-homework.ps1
+++ kustomize/kustomize-homework.ps1
@@ -1,6 +1,9 @@
 #Dev: 
+kubectl create namespace hw4-dev
 kubectl apply -k kustomize/overlays/dev
 #Test: 
+kubectl create namespace hw4-test
 kubectl apply -k kustomize/overlays/test
 #Prod: 
+kubectl create namespace hw4-prod
 kubectl apply -k kustomize/overlays/prod

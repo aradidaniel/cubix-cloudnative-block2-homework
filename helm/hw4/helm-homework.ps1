eda0bceea1470ce70d162c562465954d463bdb23
aradidaniel <106033926+aradidaniel@users.noreply.github.com>
2024-06-23 14:31:49 +0200

    fix helm and kustomize issues

--- helm/hw4/helm-homework.ps1
+++ helm/hw4/helm-homework.ps1
@@ -1,6 +1,15 @@
 #Dev: 
-helm install hw4-dev . --values values-dev.yaml 
+kubectl create namespace hw4-dev
+helm install hw4-dev . -n hw4-dev
+
 #Test: 
-helm install hw4-test . --values values-test.yaml 
+kubectl create namespace hw4-test
+helm install hw4-test . --values values.yaml --values ../values-test.yaml -n hw4-test
+
+
+
+
+
 #Prod: 
-helm install hw4-prod . --values values-prod.yaml 
+kubectl create namespace hw4-prod
+helm install hw4-prod . --values values.yaml --values ../values-prod.yaml -n hw4-prod
